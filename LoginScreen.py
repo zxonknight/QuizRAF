52d043de03e5fa56429c2ccbe8172593dc6810da
zxonknight <99401371+zxonknight@users.noreply.github.com>
2023-10-13 13:59:31 +0100

    Update LoginScreen.py

--- LoginScreen.py
+++ LoginScreen.py
@@ -10,6 +10,36 @@ def login():
     else:
         messagebox.showerror("Login Failed", "Invalid username or password")
 
+# CODE TO REGISTER NEW USER
+# Ask ben how to get it to save the new user info
+def register():
+    register_window = tk.Toplevel(root)
+    register_window.title("Register")
+
+    label_new_username = tk.Label(register_window, text="New Username:")
+    label_new_username.pack(pady=(10,0))
+    entry_new_username = tk.Entry(register_window)
+    entry_new_username.pack(pady=(0,10))
+
+    label_new_password = tk.Label(register_window, text="New Password:")
+    label_new_password.pack(pady=(10,0))
+    entry_new_password = tk.Entry(register_window, show="*")
+    entry_new_password.pack(pady=(0,10))
+
+    def register_user():
+        new_username = entry_new_username.get()
+        new_password = entry_new_password.get()
+
+        # How to save new user every time?
+
+        messagebox.showinfo("Registration Successful", "Welcome, " + new_username + "!")
+        register_window.destroy()
+
+    register_button = tk.Button(register_window, text="Register", command=register_user)
+    register_button.pack(pady=(10,20))
+
+
+
 # Create the main window
 root = tk.Tk()
 root.title("Login Screen")
